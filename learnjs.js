f1ea8de4cd780654f16a0bb15e821591428d576d
vikasrohra <vikasrohra2@gmail.com>
2024-04-08 21:38:28 +0530

    Predit the output code

---
+++ learnjs.js
@@ -0,0 +1,206 @@
+// var x = 7;
+
+// function getName () {
+//     console.log("Namaste JS");
+//     console.log("Namaste JS");
+// }
+
+// getName();
+// console.log(x);
+
+
+// getName();
+// console.log(x);
+
+// var x = 7;
+
+// function getName () {
+//     console.log("Namaste JS");
+// }
+
+
+// getName();
+// console.log(x);
+
+// function getName () {
+//     console.log("Namaste JS");
+// }
+
+
+
+// console.log(x);
+// getName();
+
+// function getName () {
+//     console.log("Namaste JS");
+// }
+
+
+// getName();
+// console.log(x);
+
+// var x = 7;
+
+// var getName = () => {
+//     console.log("Namaste JS");
+// }
+
+
+// getName();
+// console.log(x);
+
+// var x = 7;
+
+// var getName = function() {
+//     console.log("Namaste JS");
+// }
+
+
+// var x = 1;
+// a();
+// b();
+// console.log(1)
+
+// function a() {
+//     var x = 10;
+//     console.log(x);
+// }
+
+// function b() {
+//     var x = 100;
+//     console.log(x);
+// }
+
+
+// function a() {
+//     console.log(b);
+// }
+// var b = 10;
+// a();
+
+// // Output:
+// //10
+
+
+// function a() {
+//     c();
+//     function c() {
+//         console.log(b);
+//     }
+// }
+// var b = 10;
+// a();
+
+// // Output:
+// //10
+
+
+// function a() {
+//     var b = 10;
+//     c();
+//     function c() {
+//         console.log(b);
+//     }
+// }
+// a();
+
+// // Output:
+// //10
+
+
+// function a() {
+//     var b = 10;
+//     c();
+//     function c() {
+//     }
+// }
+// a();
+// console.log(b);
+
+// // Output:
+// // Uncaught ReferenceError: b is not defined
+
+
+// console.log(b);
+// let a = 10;
+// var b = 100;
+
+// // Output:
+// // undefined
+
+// let a = 10;
+// var b = 100;
+// console.log(b);
+
+// // Output:
+// // 100
+
+
+// console.log(a);
+// let a = 10;
+// var b = 100;
+
+// // Output:
+// // Uncaught ReferenceError: Cannot access 'a' before initialization
+
+
+// let a = 10;
+// var b = 100;
+// console.log(a);
+
+// // Output:
+// // 10
+
+// let a = 100;
+// let a = 10;
+
+// // Output: 
+// // Uncaught SyntaxError: Identifier 'a' has already been declared
+
+
+
+// let a = 100;
+// var a = 10;
+
+// // Output: 
+// // Uncaught SyntaxError: Identifier 'a' has already been declared
+
+
+// console.log(a);
+// var a = 100;
+// var a = 10;
+// console.log(a);
+
+// // Output: 
+// // undefined
+// // 10
+
+
+// const a;
+// console.log(a);
+
+// // Output: 
+// // Uncaught SyntaxError: Missing initializer in const declaration
+
+// const a;
+// a = 10;
+// console.log(a);
+
+// // Output: 
+// // Uncaught SyntaxError: Missing initializer in const declaration
+
+
+// console.log("Namaste");
+// const a = 1;
+// a = 10;
+// console.log(a);
+
+// // Output: 
+// // Uncaught TypeError: Assignment to constant variable.
+
+
+const a = 1;
+console.log(a);
+
+// Output: 
+// 1
